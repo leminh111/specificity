617d24837914ce0df9c8e2b64637d0fc933cb58b
moe <leminh111@gmail.com>
2015-12-11 22:49:41 +0000

    update regex

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+parser('button.btn.btn-primary[data-select="link"] button#btn1:hover > span::first-letter#abc:hover::first-line');
 function parser(selector) {
   var result = {
     raw: selector,
@@ -9,19 +11,21 @@ function parser(selector) {
     segments: []
   };
 
+  // TODO append special character like > ~ \s
+  // pseudo class [^\:](\:[a-z0-9-_]+)
   var rgxClasses = '(\\.[a-z0-9-_]+)',
       rgxIds = '(#[a-z0-9-_]+)',
       rgxPseuClasses = '(\\:[a-z0-9-_]+)',
-      rgxEl = '(\\s[a-z0-9-_]+)',
-      rgxElBegin = '(^[a-z0-9-_]+)'
+      rgxEl = '([a-zA-Z0-9]+)',
       rgxPseuEl = '(\\:\\:[a-z0-9-_]+)',
       rgxAttr = '(\\[[^>:#.\\s]+\\])',
+      rgxNone = '(\\s+)|(>)|(~)',
 
-      rgxTotal = rgxClasses + '|' + rgxIds + '|' + rgxPseuClasses + '|' + rgxEl + '|' + rgxElBegin + '|' + rgxPseuEl + '|' + rgxAttr;
+      rgxTotal = rgxClasses + '|' + rgxIds + '|' + rgxPseuClasses + '|' + rgxEl + '|' + rgxPseuEl + '|' + rgxAttr + '|' + rgxNone;
 
   var rex = new RegExp(rgxTotal, 'g'),
 
-      rgxType0 = rgxEl + '|' + rgxElBegin + '|' + rgxPseuEl,
+      rgxType0 = rgxEl + '|' + rgxPseuEl,
       rexType0 = new RegExp(rgxType0);
 
       rgxType1 = rgxClasses + '|' + rgxAttr + '|' + rgxPseuClasses,
@@ -29,6 +33,9 @@ function parser(selector) {
 
       rgxType2 = rgxIds,
       rexType2 = new RegExp(rgxType2);
+
+      rgxTypeNone = rgxNone,
+      rexTypeNone = new RegExp(rgxTypeNone);
 
   while (matched = rex.exec(selector)) {
     if (rexType0.exec(matched[0].toString())) {
@@ -49,9 +56,28 @@ function parser(selector) {
         type: 2
       }
       result.types[2].push(obj.selector);
+    } else if (rexTypeNone.exec(matched[0].toString())) {
+      var obj = {
+        selector: matched[0].toString(),
+        type: 'none'
+      }
     }
 
     result.segments.push(obj);
   }
+
+  // Create a loop to append each selector to the text-field
+//  var textField = document.getElementsByClassName("text-field")[0];
+//
+//  for (var i=0; i<result.segments.length; i++) {
+//    var span = document.createElement("SPAN");
+//    span.className = 'type-' + result.segments[i].type;
+//    var selectorName = result.segments[i].selector;
+//    var t = document.createTextNode(selectorName);
+//    span.appendChild(t);
+//    textField.appendChild(span);
+//  }
+  console.log(result);
   return result;
 }
+});
--- src/view.js
+++ src/view.js
@@ -1,51 +1,100 @@
-var Type2Cal = React.createClass({
-    render: function() {
-        return (
-          <div>
-          </div>
-        );
-    }
+var SelectorSpan = React.createClass({
+  render: function() {
+    var className = 'type-' + this.props.type;
+
+    return (
+      <span className={className}>
+        {this.props.children}
+      </span>
+    );
+  }
 });
 
-var SpecDup = React.createClass({
-    render: function() {
-        return (
-          <a />
-        );
-    }
+var TextField = React.createClass({
+  getInitialState: function() {
+    return {input: ''};
+  },
+  handleChange: function(e) {
+    this.setState({input: e.target.value});
+  },
+
+  render: function() {
+    var SelectorSpanNodes = this.props.data.segments.map(function(ss) {
+      return (
+        <SelectorSpan type={ss.type}>
+          {ss.selector}
+        </SelectorSpan>
+      );
+    });
+    return (
+      <div className="text-field">
+        <input className="event-capture" value={this.state.input} onChange={this.handleChange} />
+        {SelectorSpanNodes}
+      </div>
+    );
+  }
 });
 
-var SpecDisplay = React.createClass({
-    render: function() {
-        return (
-          <Type2Cal />
-          //<Type1Cal />
-          //<Type0Cal />
-        );
-    }
+var BoxType = React.createClass({
+  render: function() {
+    var className="box type-" + this.props.type;
+    return (
+      <div className={className}>
+        <div className="inner">
+          <span className="number">{this.props.number}</span>
+          <span className="description">{this.props.desc}</span>
+        </div>
+      </div>
+    );
+  }
 });
 
-var SpecInput = React.createClass({
-    render: function() {
-        return (
-          <textarea className="input" type="text" />
-        );
-    }
+var ListBox = React.createClass({
+  render: function() {
+    return (
+      <div className="list-box">
+        <BoxType type="2" desc="IDs"
+          number={this.props.data.types[2].length} />
+        <BoxType type="1" desc="classes"
+          number={this.props.data.types[1].length} />
+        <BoxType type="0" desc="el"
+          number={this.props.data.types[0].length} />
+      </div>
+    );
+  }
 });
 
-var SpecificityBox = React.createClass({
+var SpecifictyTable = React.createClass({
     render: function() {
-        return (
-            <div className="item">
-                <SpecInput />
-                <SpecDisplay />
-                <SpecDup />
-            </div>
-        );
+      this.data = {
+        raw: 'button.btn#btn1:hover',
+        types: {
+          0:['button'],
+          1:['.btn',':hover'],
+          2:['#btn1']
+        },
+        segments:[
+          {selector: 'button',type:0},
+          {selector: '.btn', type:1},
+          {selector: '#btn1', type:2},
+          {selector:':hover', type:1}
+        ]
+      };
+
+      return (
+        <div className="content-box">
+          <TextField data={this.data}/>
+          <ListBox data={this.data}/>
+        </div>
+      );
     }
 });
 
 ReactDOM.render(
-  <SpecificityBox />,
-  document.getElementById('content')
+  <div>
+    <SpecifictyTable />
+    <SpecifictyTable />
+    <SpecifictyTable />
+  </div>,
+    document.getElementById('container')
 );
